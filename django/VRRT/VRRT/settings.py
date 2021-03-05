4a3ad41b5a643cbf8052f444fc8b87fba6777c84
stoldalc <christianstoldal@gmail.com>
2021-03-05 12:11:55 -0800

    Attempting to track down the static files location issues

--- django/VRRT/VRRT/settings.py
+++ django/VRRT/VRRT/settings.py
@@ -150,15 +150,25 @@ DATABASES['default'].update(db_from_env)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# # The absolute path to the directory where collectstatic will collect static files for deployment.
+# STATIC_ROOT = 'django/VRRT/VRRTController/static/'  #. os.path.join(BASE_DIR, 'staticfiles')
+
+# # The URL to use when referring to static files (where they will be served from)
+# STATIC_URL = '/static/'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = 'django/VRRT/VRRTController/static/'  #. os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'  #. os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 
 
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# # Simplified static file serving.
+# # https://warehouse.python.org/project/whitenoise/
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
